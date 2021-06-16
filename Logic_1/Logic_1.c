5214e390110f84e7b577a8a26cb4d4ffd110b764
MinHyukLee <boss5251@naver.com>
2021-06-16 15:03:36 +0900

    Decimal to Hexadecimal Conclusion

--- Logic_1/Logic_1.c
+++ Logic_1/Logic_1.c
@@ -4,11 +4,57 @@
 
 /* It's about how to convert Decimal to Hexadecimal */
 
+int print_hex(int value)
+{
+    int mod = 0;
+    int decimal = 0;
+    int position = 0;
+    char hexa[20] = {0, };
+
+    decimal = value;
+
+    while(1)
+    {
+
+        mod = decimal % 16;
+
+        if(mod < 10)
+        {
+            hexa[position] = 48 + mod;
+        }else
+        {
+            hexa[position] = 65 + (mod - 10);
+        }
+
+        decimal = decimal / 16;
+
+        position++;
+
+        if(decimal == 0)
+        {
+            break;
+        }
+    }
+
+    printf("Decimal : %d convert Hexadecimal = ", value);
+
+    for(int i = position-1; i>=0; i--)
+    {
+        printf("%c", hexa[i]);
+    }
+
+    printf("\n");
+
+    return 0;
+}
+
 int main()
 {
     char Input[100];
     int Decimal[100];
     int count = 0;
+    int ret = 0;
+    int i, j;
 
     char *token;
 
@@ -17,7 +63,7 @@ int main()
     fgets(Input, 100, stdin);   /* Using fgest becaus of space bar (gets func don't recognize space bar) */
 
     /* Counting decimal value numbers */
-    for(int i=0; Input[i] != '\0'; i++)
+    for(i = 0; Input[i] != '\0'; i++)
     {
         if(Input[i] == ',')
         {
@@ -33,7 +79,7 @@ int main()
 
     /* Store Decimal Value in int array using strtok, token = ',' */
     token = strtok(Input, ",");
-    for(int i=0; i<count; i++)
+    for(i = 0; i<count; i++)
     {
         Decimal[i] = atoi(token);
 
@@ -48,8 +94,12 @@ int main()
         printf("The Number of Input Decimal : %d\n", count);
     }
 
-    
+    /* Calculate Hexadecimal using Decimal Array */
 
+    for(i = 0; i<count; i++)
+    {
+        ret = print_hex(Decimal[i]);
+    }
     
     return 0;
 }
